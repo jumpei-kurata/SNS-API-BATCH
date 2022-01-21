c129a3ede8a102cce558be2d674497a52768efbc
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2022-01-21 10:11:39 +0900

    ユーザー登録時に論理IDを自動生成し、DBに登録する機能を実装しました。

--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -1,4 +1,5 @@
 package com.example.domain;
+
 import java.sql.Date;
 import java.time.LocalDateTime;
 
@@ -20,106 +21,147 @@ public class User {
 	private String userPhotoPath;
 	private Date birthDay;
 	private String introduction;
+	private String logicalId;
 	private LocalDateTime loginedTime;
 	private LocalDateTime updatedTime;
 	private LocalDateTime registeredTime;
 	private Boolean deleted;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAccountName() {
 		return accountName;
 	}
+
 	public void setAccountName(String accountName) {
 		this.accountName = accountName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Date getHireDate() {
 		return hireDate;
 	}
+
 	public void setHireDate(Date hireDate) {
 		this.hireDate = hireDate;
 	}
+
 	public Integer getServiceFk() {
 		return serviceFk;
 	}
+
 	public void setServiceFk(Integer serviceFk) {
 		this.serviceFk = serviceFk;
 	}
+
 	public String getServiceName() {
 		return serviceName;
 	}
+
 	public void setServiceName(String serviceName) {
 		this.serviceName = serviceName;
 	}
+
 	public String getUserPhotoPath() {
 		return userPhotoPath;
 	}
+
 	public void setUserPhotoPath(String userPhotoPath) {
 		this.userPhotoPath = userPhotoPath;
 	}
+
 	public Date getBirthDay() {
 		return birthDay;
 	}
+
 	public void setBirthDay(Date birthDay) {
 		this.birthDay = birthDay;
 	}
+
 	public String getIntroduction() {
 		return introduction;
 	}
+
 	public void setIntroduction(String introduction) {
 		this.introduction = introduction;
 	}
+
+	public String getLogicalId() {
+		return logicalId;
+	}
+
+	public void setLogicalId(String logicalId) {
+		this.logicalId = logicalId;
+	}
+
 	public LocalDateTime getLoginedTime() {
 		return loginedTime;
 	}
+
 	public void setLoginedTime(LocalDateTime loginedTime) {
 		this.loginedTime = loginedTime;
 	}
+
 	public LocalDateTime getUpdatedTime() {
 		return updatedTime;
 	}
+
 	public void setUpdatedTime(LocalDateTime updatedTime) {
 		this.updatedTime = updatedTime;
 	}
+
 	public LocalDateTime getRegisteredTime() {
 		return registeredTime;
 	}
+
 	public void setRegisteredTime(LocalDateTime registeredTime) {
 		this.registeredTime = registeredTime;
 	}
+
 	public Boolean getDeleted() {
 		return deleted;
 	}
+
 	public void setDeleted(Boolean deleted) {
 		this.deleted = deleted;
 	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", accountName=" + accountName + ", email=" + email + ", password="
 				+ password + ", hireDate=" + hireDate + ", serviceFk=" + serviceFk + ", serviceName=" + serviceName
 				+ ", userPhotoPath=" + userPhotoPath + ", birthDay=" + birthDay + ", introduction=" + introduction
-				+ ", loginedTime=" + loginedTime + ", updatedTime=" + updatedTime + ", registeredTime=" + registeredTime
-				+ ", deleted=" + deleted + "]";
+				+ ", logicalId=" + logicalId + ", loginedTime=" + loginedTime + ", updatedTime=" + updatedTime
+				+ ", registeredTime=" + registeredTime + ", deleted=" + deleted + "]";
 	}
+
 }
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -17,136 +17,142 @@ import com.example.repository.UserRepository;
 
 /**
  * ユーザーのためのserviceです
+ * 
  * @author ootomokenji
  *
  */
 @Service
 @Transactional
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
 	private MailRepository mailRepository;
 	@Autowired
 	private MailSender sender;
-	
-	//このアドレスからメールを送ります
+
+	// このアドレスからメールを送ります
 	private final String FROMEMAIL = "kenji.otomo@rakus-partners.co.jp";
-	
+
 	/**
 	 * IDからユーザー情報を取得します
+	 * 
 	 * @param user IDだけがセットされたUserドメイン
 	 * @return 情報が格納されたUserドメイン
 	 */
 	public User findById(User user) {
 		return userRepository.findById(user);
 	}
-	
+
 	/**
 	 * Emailからユーザー情報を取得します
+	 * 
 	 * @param form EmailとpasswordがセットされたLoginForm
 	 * @return 情報が格納されたUserドメイン
 	 */
 	public User findByEmail(User user) {
-		
+
 		List<User> users = userRepository.findByEmail(user);
 		if (users.size() == 0) {
 			return null;
-		}else {
+		} else {
 			user = users.get(0);
 			return user;
 		}
 	}
-	
+
 	/**
 	 * ログイン処理します
+	 * 
 	 * @param user
 	 * @param form
 	 * @return
 	 */
-	public User login(User user,LoginForm form) {
-		
-		if( user.getPassword() .equals ( form.getPassword() ) ) {
-			
+	public User login(User user, LoginForm form) {
+
+		if (user.getPassword().equals(form.getPassword())) {
+
 			userRepository.loginedUpdate(user);
 			user = userRepository.findById(user);
 			return user;
-		}else {
+		} else {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * ユーザー情報を登録します
+	 * 
 	 * @param user 入力されたユーザー情報
 	 * @return ユーザー情報
 	 */
 	public User insertUser(User user) {
 
-		//メールアドレス重複チェック
-		List<User>list = userRepository.findByEmail(user);
-		
+		// メールアドレス重複チェック
+		List<User> list = userRepository.findByEmail(user);
+
 		if (list.size() != 0) {
 			return null;
-		}else {
+		} else {
 			String userPhotoPath = createUserPhotoPath();
 			user.setUserPhotoPath(userPhotoPath);
-			
+
 			userRepository.insertUser(user);
-			return user;		
+			return user;
 		}
 	}
-	
+
 	/**
 	 * ランダムにユーザーの写真パスを生成します
 	 * 
 	 * @return
 	 */
 	public String createUserPhotoPath() {
-		
-		//写真の枚数
+
+		// 写真の枚数
 		Integer photoNumber = 6;
 
-		int randomPathNumber = (int)(Math.random() * photoNumber) + 1;
+		int randomPathNumber = (int) (Math.random() * photoNumber) + 1;
 		String userPhotoPath = "user" + randomPathNumber + ".jpeg";
-		
+
 		return userPhotoPath;
 	}
 
 	/**
 	 * ユーザー情報を更新します
+	 * 
 	 * @param user 入力されたユーザー情報
 	 * @return ユーザー情報
 	 */
 	public User updateUser(User user) {
-		
+
 		User beforeUser = userRepository.findById(user);
-		
+
 		if (user.getName() != null) {
 			beforeUser.setName(user.getName());
 		}
-		
+
 		if (user.getAccountName() != null) {
 			beforeUser.setAccountName(user.getAccountName());
 		}
-		
+
 		if (user.getHireDate() != null) {
 			beforeUser.setHireDate(user.getHireDate());
 		}
-		
+
 		if (user.getServiceFk() != null) {
 			beforeUser.setServiceFk(user.getServiceFk());
 		}
-		
+
 		if (user.getBirthDay() != null) {
 			beforeUser.setBirthDay(user.getBirthDay());
 		}
-		
+
 		if (user.getIntroduction() != null) {
 			beforeUser.setIntroduction(user.getIntroduction());
 		}
-		
+
 		userRepository.updateUser(beforeUser);
 		user = userRepository.findById(beforeUser);
 		return user;
@@ -159,96 +165,118 @@ public class UserService {
 	 * @return
 	 */
 	public User updatePassword(User user) {
-		
-		List<User>userList = userRepository.findByEmail(user);
-		
-		if(userList.size() != 1) {
+
+		List<User> userList = userRepository.findByEmail(user);
+
+		if (userList.size() != 1) {
 			return null;
 		}
-		
+
 		String password = user.getPassword();
 		user = userList.get(0);
 		user.setPassword(password);
 
 		userRepository.updateUser(user);
 		user = userRepository.findById(user);
-		
+
 		return user;
 	}
-	
+
 	/**
 	 * ユーザー仮登録時にメールアドレス認証のメールを送ります
 	 * 
 	 * @param mail
 	 */
 	public void accountConfirmMail(Mail mail) {
-		
-		List<Mail>list = mailRepository.findMailByEmail(mail);
+
+		List<Mail> list = mailRepository.findMailByEmail(mail);
 		String token = createToken();
 
 		mail.setToken(token);
 		mail.setStatus(0);
-		
+
 		if (list.size() == 0) {
 			mailRepository.insertMail(mail);
-		}else {
+		} else {
 			mailRepository.changeTokenMail(mail);
 		}
-		
+
 		SimpleMailMessage msg = new SimpleMailMessage();
 		try {
 			msg.setFrom(FROMEMAIL);
 			msg.setTo(mail.getEmail());
 			msg.setSubject("メールアドレス認証のお願い");
-			msg.setText(mail.getName()+" 様\nURLです\n\n"+"http://localhost:8080/" + token);
+			msg.setText(mail.getName() + " 様\nURLです\n\n" + "http://localhost:8080/" + token);
 			sender.send(msg);
 		} catch (Exception e) {
-				e.printStackTrace();
+			e.printStackTrace();
 		}
 		mail.setStatus(1);
 		mailRepository.changeStatusMail(mail);
 	}
-	
+
 	/**
 	 * パスワード変更時にメールを送信します
 	 * 
 	 * @param user
 	 */
 	public void changePasswordMail(User user) {
-		
+
 		Mail mail = new Mail();
 		String token = createToken();
-		
+
 		mail.setEmail(user.getEmail());
 		mail.setToken(token);
-		
+
 		mailRepository.changeTokenMail(mail);
-		
+
 		SimpleMailMessage msg = new SimpleMailMessage();
 		try {
 			msg.setFrom(FROMEMAIL);
 			msg.setTo(user.getEmail());
 			msg.setSubject("パスワードの再設定について");
-			msg.setText(user.getName() + "様\n\n" + "パスワードの再設定がリクエストされました\n" +
-						"以下のリンクから再設定が可能です。\n\n" +
-						"http://localhost:8080/" + token + "\n\n" +
-						"このメールに心当たりが無い場合は無視してください。\n" +
-						"上記URLを通して再設定しない限り、パスワードは変更されません。\n");
+			msg.setText(user.getName() + "様\n\n" + "パスワードの再設定がリクエストされました\n" + "以下のリンクから再設定が可能です。\n\n"
+					+ "http://localhost:8080/" + token + "\n\n" + "このメールに心当たりが無い場合は無視してください。\n"
+					+ "上記URLを通して再設定しない限り、パスワードは変更されません。\n");
 			sender.send(msg);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	/**
 	 * 32桁の認証用トークンを発行します
 	 * 
 	 * @return
 	 */
 	public String createToken() {
-		
+
 		byte tokenSize[] = new byte[16];
+		StringBuffer token = new StringBuffer();
+
+		try {
+			SecureRandom random = new SecureRandom();
+			random.nextBytes(tokenSize);
+			for (int i = 0; i < tokenSize.length; i++) {
+				token.append(String.format("%02x", tokenSize[i]));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+
+		return token.toString();
+	}
+	
+	/**
+	 * 16桁の認証用トークンを発行します
+	 * 
+	 * @return
+	 */
+	public String createHalfToken() {
+		
+		byte tokenSize[] = new byte[8];
 		StringBuffer token = new StringBuffer();
 		
 		try {
@@ -264,7 +292,7 @@ public class UserService {
 		
 		return token.toString();
 	}
-	
+
 	/**
 	 * トークンによってメールを検索
 	 * 
@@ -272,10 +300,10 @@ public class UserService {
 	 * @return
 	 */
 	public List<Mail> findMailByToken(Mail mail) {
-		List<Mail>list = mailRepository.findMailByToken(mail);
+		List<Mail> list = mailRepository.findMailByToken(mail);
 		return list;
 	}
-	
+
 	/**
 	 * メールによってメールを検索
 	 * 
@@ -283,10 +311,10 @@ public class UserService {
 	 * @return
 	 */
 	public List<Mail> findMailByEmail(Mail mail) {
-		List<Mail>list = mailRepository.findMailByEmail(mail);
+		List<Mail> list = mailRepository.findMailByEmail(mail);
 		return list;
 	}
-	
+
 	/**
 	 * メールテーブルのステータスを変更します
 	 * 
@@ -297,7 +325,7 @@ public class UserService {
 		mailRepository.changeStatusMail(mail);
 		return mail;
 	}
-	
+
 	/**
 	 * メールテーブルのトークンを更新します
 	 * 
@@ -309,10 +337,10 @@ public class UserService {
 		String newAccountToken = createToken();
 		mail.setToken(newAccountToken);
 		mailRepository.changeTokenMail(mail);
-		
+
 		return mail;
 	}
-	
+
 	/**
 	 * メールテーブルにinsertします
 	 * 
@@ -323,4 +351,33 @@ public class UserService {
 		mailRepository.insertMail(mail);
 		return mail;
 	}
+
+	
+	/**
+	 * DBにかぶりがないlogicalIdをuserに付与します
+	 * 
+	 * @param user
+	 * @return logicalIdが付与されたuser
+	 */
+	public User generateLogicalId(User user) {
+		boolean logicalIdIsUnique = false; 
+		
+		// DBに同じ論理IDがいなくなるまで、トークン生成を行う
+		while(!logicalIdIsUnique) {
+			System.out.println("生成");
+			user.setLogicalId(createHalfToken());
+		
+			// 生成した論理IDでDBを検索し、該当するものがなければ、isUniqueをTRUEに
+			System.out.println("検索結果:"+
+					userRepository.findByLogicalId(user)== null);
+			
+			if(userRepository.findByLogicalId(user) == null) {
+				logicalIdIsUnique = true ;
+			}
+		}
+		
+		return user;
+	}
+	
+	
 }
