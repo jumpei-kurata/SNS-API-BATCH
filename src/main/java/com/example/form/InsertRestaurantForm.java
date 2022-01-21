dbb12ecf9172c0804aa0fd3e5e8d67375af9bc9c
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2022-01-21 11:12:35 +0900

    ホットペッパーからのデータをもとにレストランを登録できる機能を実装しました。 ・【DDL】restaurantsテーブル、photopathのカラムをvarchar→textに変更 ・【Controller】コメントアウトしていた部分を復活 ・【Form/ByHotpepprForm】提供タイプのバリデーション方法を変更、不要なインポートを削除 ・【Service】サービスに渡されたときに画像パスがnullの場合（手書き入力の場合）のみ、ジャンルに合わせた画像パスを付与するように変更

--- src/main/java/com/example/form/InsertRestaurantForm.java
+++ src/main/java/com/example/form/InsertRestaurantForm.java
@@ -2,7 +2,6 @@ package com.example.form;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 import lombok.Data;
 
