c7ccf870a429c997d8099d1c5900ac77be445c24
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2022-02-01 01:07:01 +0900

    通知を表示する機能を実装しました。

---
+++ src/main/java/com/example/service/NotificationService.java
@@ -0,0 +1,38 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Notification;
+import com.example.domain.User;
+import com.example.repository.LikeCommentRepository;
+
+@Service
+@Transactional
+public class NotificationService {
+
+	@Autowired
+	private LikeCommentRepository likeCommentRepository;
+	
+// 汎用	
+	
+	/**
+	 * 渡したuserデータのIDに沿って、そのユーザーへの通知リストを表示するサービス
+	 * 
+	 * @param user
+	 * @return
+	 */
+	public List<Notification> showNotificationListByUser(User user){
+		// そのユーザーへの通知を取得
+		List<Notification> list = likeCommentRepository.findNotificationByUserId(user);
+		
+		// 現時点でDBに格納されているnoticed カラムを更新
+		// 未実装
+		
+		return list ;
+	}
+	
+}
